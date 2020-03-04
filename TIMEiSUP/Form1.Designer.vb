13883e5d16577f09b08b1af08ba2ec9e20a1fdea
Takuchan <awatykhall850@outlook.com>
2020-03-04 16:19:39 +0900

    とりあえずレイアウトと簡単なコードは書いた
    いや、初めての言語よくわからなすぎ。。。
    タイマーとテキストボックス、テキストの宣言及びIntの変換は完了。
    タイマーの使い方がわからない。

---
+++ TIMEiSUP/Form1.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.hintbutton = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(524, 324)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(271, 83)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "スタート"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("MS UI Gothic", 30.0!)
+        Me.TextBox1.Location = New System.Drawing.Point(12, 230)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(68, 47)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(87, 265)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "H"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("MS UI Gothic", 30.0!)
+        Me.TextBox2.Location = New System.Drawing.Point(106, 230)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(68, 47)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(180, 265)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(14, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "M"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("MS UI Gothic", 30.0!)
+        Me.TextBox3.Location = New System.Drawing.Point(200, 230)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(68, 47)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(274, 265)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(12, 12)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "S"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(443, 384)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "リセット"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Font = New System.Drawing.Font("MS UI Gothic", 30.0!)
+        Me.TextBox7.Location = New System.Drawing.Point(403, 230)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(68, 47)
+        Me.TextBox7.TabIndex = 9
+        Me.TextBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(478, 265)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 12)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "H"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Font = New System.Drawing.Font("MS UI Gothic", 30.0!)
+        Me.TextBox8.Location = New System.Drawing.Point(497, 230)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(68, 47)
+        Me.TextBox8.TabIndex = 11
+        Me.TextBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(571, 265)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(14, 12)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "M"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Font = New System.Drawing.Font("MS UI Gothic", 30.0!)
+        Me.TextBox9.Location = New System.Drawing.Point(591, 230)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(68, 47)
+        Me.TextBox9.TabIndex = 13
+        Me.TextBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(665, 265)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(12, 12)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "S"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(294, 257)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 12)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "集中したら..."
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(692, 257)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 12)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "休憩する"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 384)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(304, 23)
+        Me.RichTextBox1.TabIndex = 17
+        Me.RichTextBox1.Text = "やる気を一言"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Location = New System.Drawing.Point(89, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(614, 168)
+        Me.GroupBox1.TabIndex = 18
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "タイマー"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("MS UI Gothic", 10.0!)
+        Me.Label6.Location = New System.Drawing.Point(251, 76)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(79, 14)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "00:00:00:000"
+        '
+        'hintbutton
+        '
+        Me.hintbutton.Location = New System.Drawing.Point(719, 295)
+        Me.hintbutton.Name = "hintbutton"
+        Me.hintbutton.Size = New System.Drawing.Size(75, 23)
+        Me.hintbutton.TabIndex = 19
+        Me.hintbutton.Text = "ヒント"
+        Me.hintbutton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(807, 419)
+        Me.Controls.Add(Me.hintbutton)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "TIMEiSUP!"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents hintbutton As Button
+End Class
---
+++ TIMEiSUP/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub hintbutton_Click(sender As Object, e As EventArgs) Handles hintbutton.Click
+        MessageBox.Show("何も入力しない状態でどれかボタンを押すべし。そうすると新機能がわかる",
+                "アプリのヒント",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim timer As Timer = New Timer()
+        Dim strhour As String
+        Dim strminute As String
+        Dim strsecond As String
+
+        Dim inthour As Integer
+        Dim intminute As Integer
+        Dim intsecond As Integer
+
+
+        ' strhour = TextBox1.Text
+        'strminute = TextBox2.Text
+        strsecond = TextBox3.Text
+
+        ' inthour = Integer.Parse(strhour)
+        'intminute = Integer.Parse(strminute)
+        intsecond = Integer.Parse(strsecond)
+
+        intsecond = intsecond * 1000
+
+        timer.Interval = intsecond
+        timer.Enabled = True
+
+
+        Label6.Text = timer.ToString
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
